76bc7904d32f57c47b81f7041db0456b547eca50
manonymous2 <esther@manonymous.com>
2013-12-28 23:58:05 -0500

    added model db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131228002705) do
+ActiveRecord::Schema.define(:version => 20131229045059) do
 
   create_table "customers", :force => true do |t|
     t.string   "app_name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "events", :force => true do |t|
+    t.string   "event_name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
