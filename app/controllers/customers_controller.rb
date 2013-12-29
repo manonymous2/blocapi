bea3d40fef5a062a37660e94dfdb5282488962b1
vagrant <vagrant@rails-dev-box>
2013-12-29 03:05:01 +0000

    First commit

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,61 @@
+class CustomersController < ApplicationController
+
+
+  def index
+    @customers = Customer.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def create
+    @customer = Customer.create(params[:customer])
+
+    respond_to do |format|
+        format.html { redirect_to customers_path, notice: 'Customer was successfully created.' }
+        format.js { render :create }
+    end
+  end
+
+  # PUT /customers/1
+  # PUT /customers/1.json
+  def update
+    @customer = Customer.find(params[:id])
+
+    respond_to do |format|
+      if @customer.update_attributes(params[:customer])
+        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /customers/1
+  # DELETE /customers/1.json
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+
+    respond_to do |format|
+      format.html { redirect_to customers_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocapi::Application.config.secret_token = 'e5804fe865f9606817499bbce9c11280f01bfe9f16609604f96227e0d89ef3ee06859e088bb4e13e792f0419dda89df012f1e2d03e58e09d62e8a028d697ba53'
